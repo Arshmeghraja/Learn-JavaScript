38d1b33ffebe3f49cba276b7e83e139bf95e6deb
Arshmeghraja <meghrejaarsh@gmail.com>
2024-12-08 10:18:40 +0000

    Saved

---
+++ Basics /Variables.js
@@ -0,0 +1,7 @@
+ const account_Id = "ArshMeghraja"
+let account_Email = "arshmeg@gmail.com"
+var account_type = "12345 "
+ accountNo = 24651
+ account_Email = "arshmeghrega@gmail.com"
+ account_type = "2468 "
+console.table([account_Id,account_Email,account_type,accountNo]);
